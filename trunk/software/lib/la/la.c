2ff7069b4a2e427270f9979cb9043354131d5563
tmbinc@elitedvb.net <tmbinc@elitedvb.net@6f03505e-0e28-11de-98bb-07cc9828235c>
2009-03-11 18:14:39 +0000

    initial checkins of core, s3a-starterkit port and software libs

---
+++ trunk/software/lib/la/la.c
@@ -0,0 +1,48 @@
+#include "la.h"
+
+void la_init(struct la_state *la, void *npi_la, void *mem_base, int mem_offset, int size)
+{
+	la->npi_la = npi_la;
+	la->mem = mem_base;
+	la->offset = mem_offset;
+	la->size = size;
+
+	la->trigger0 = 0;
+	la->trigger0_mask = 0;
+	la->state_mask = 0xffffffff;
+}
+
+void la_start(struct la_state *la)
+{
+	la->npi_la[0] = la->offset;
+	la->npi_la[1] = 0;
+	la->npi_la[2] = la->size - 1;
+	la->npi_la[4] = la->trigger0;
+	la->npi_la[5] = la->trigger0_mask;
+
+	la->npi_la[6] = la->state_mask;
+
+	la->npi_la[3] = 0xFFFFFFFF; // Enable
+}
+
+int la_get_wptr(struct la_state *la)
+{
+	return la->npi_la[1] & (la->size-1);
+}
+
+void la_stop(struct la_state *la)
+{
+	la->npi_la[3] = 0;
+}
+
+void la_set_trigger(struct la_state *la, int stage, unsigned long trigger, unsigned long trigger_mask)
+{
+	la->trigger0 = trigger;
+	la->trigger0_mask = trigger_mask;
+}
+
+void la_set_state_mask(struct la_state *la, unsigned long state_mask)
+{
+	la->state_mask = state_mask;
+}
+
---
+++ trunk/software/lib/la/la.h
@@ -0,0 +1,22 @@
+#ifndef __la_h
+#define __la_h
+
+struct la_state
+{
+	int offset;
+	volatile unsigned int *npi_la;
+	volatile unsigned int *mem;
+	int size;
+	
+	unsigned long trigger0, trigger0_mask, state_mask;
+	
+};
+
+extern void la_init(struct la_state *la, void *npi_la, void *mem_base, int mem_offset, int size);
+extern void la_start(struct la_state *la);
+extern int la_get_wptr(struct la_state *la);
+extern void la_stop(struct la_state *la);
+extern void la_set_trigger(struct la_state *la, int stage, unsigned long trigger, unsigned long trigger_mask);
+extern void la_set_state_mask(struct la_state *la, unsigned long state_mask);
+
+#endif
