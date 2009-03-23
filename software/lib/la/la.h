c1e025b81366dbb2b2883213527363d3ab06ec91
tmbinc <tmbinc@elitedvb.net>
2009-03-23 03:53:27 +0000

    add wrap around handling in httpd, add pretrigger / capture stop

--- software/lib/la/la.h
+++ software/lib/la/la.h
@@ -19,14 +19,18 @@ struct la_state
 	int size;
 	
 	unsigned long trigger0, trigger0_mask, state_mask;
-	
+	unsigned long trigger_offset;
+	int autostop;
 };
 
 extern void la_init(struct la_state *la, void *npi_la, void *mem_base, int mem_offset, int size);
 extern void la_start(struct la_state *la);
 extern int la_get_wptr(struct la_state *la);
+extern int la_get_state(struct la_state *la);
+
 extern void la_stop(struct la_state *la);
 extern void la_set_trigger(struct la_state *la, int stage, unsigned long trigger, unsigned long trigger_mask);
+extern void la_set_pretrigger(struct la_state *la, int enable, int pretrigger);
 extern void la_set_state_mask(struct la_state *la, unsigned long state_mask);
 
 #endif
